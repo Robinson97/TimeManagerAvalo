f3dfac6da68425bf96aca22e50932f323352a68f
Robin Eckhardt <Robin99re@gmail.com>
2019-06-13 20:02:18 +0200

    Add project files.

---
+++ ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace TimeManagerAvalo.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeManagerAvalo.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+
+        public List<string> TmpList = new List<string>();
+
+        #region Commands
+        public ReactiveCommand CmdAddNoteToTable { get; }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            CmdAddNoteToTable = ReactiveCommand.Create(() => { Console.WriteLine("Moin"); });
+            TmpList.Add("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+            TmpList.Add("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+            TmpList.Add("UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+        }
+    }
+}
